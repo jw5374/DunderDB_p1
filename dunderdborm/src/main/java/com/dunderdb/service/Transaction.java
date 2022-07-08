894928916d179e9730f8687959d948b65cc2db5a
Bezen Giday <gidayb0921@students.bowiestate.edu>
2022-07-08 01:18:25 -0400

    interface methods added to Transaction class

--- dunderdborm/src/main/java/com/dunderdb/service/Transaction.java
+++ dunderdborm/src/main/java/com/dunderdb/service/Transaction.java
@@ -3,5 +3,36 @@ package com.dunderdb.service;
 import com.dunderdb.DunderTx;
 
 public class Transaction implements DunderTx {
+
+    String name = "first name";
+    
+    @Override
+    public String beginTransaction(){
+
+        return name;
+    }
+    @Override
+    public void processdbComands(){
+
+    }
+
+    @Override
+     public void checkForErrors(){
+
+     }
+
+     @Override
+     public void commitTransaction(){
+        
+     }
+
+
+    
+
+   
+    
+
+
+    
     
 }

95512bd87172fe6688bc44da83fd3ceb49a2df4a
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-07 23:11:03 -0400

    hacking together model processing into tables

--- dunderdborm/src/main/java/com/dunderdb/App.java
+++ dunderdborm/src/main/java/com/dunderdb/App.java
@@ -1,5 +1,8 @@
 package com.dunderdb;
 
+import com.dunderdb.annotations.Column;
+import com.dunderdb.annotations.PrimaryKey;
+import com.dunderdb.annotations.Table;
 import com.dunderdb.config.Configuration;
 
 public class App 
@@ -10,6 +13,19 @@ public class App
         config.createConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
         config.setConnectionSchema("public");
         config.setConnectionDriver("org.postgresql.Driver");
-
+        config.addAnnotatedClass(Demo.class);
+        config.setupDatabase();
     }
+}
+
+@Table(name = "example")
+class Demo {
+    @PrimaryKey(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
 }

1feb6be60fa31442a93fadc7e60107f7abb9c40d
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-09 16:50:00 -0400

    potentially able to configure with properties file

--- dunderdborm/src/main/java/com/dunderdb/config/Configuration.java
+++ dunderdborm/src/main/java/com/dunderdb/config/Configuration.java
@@ -1,11 +1,14 @@
 package com.dunderdb.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -18,6 +21,35 @@ import com.dunderdb.util.SQLConverter;
 public class Configuration {
     
     private List<ClassModel<Class<?>>> modelsList;
+
+    public Configuration() { }
+
+    public Configuration(String configName) throws IOException {
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(configName);
+        Properties prop = new Properties();
+        prop.load(in);
+        String url, user, pass, schema, driver, maxIdle, maxOpen;
+        url = prop.getProperty("DB_URL");
+        user = prop.getProperty("DB_USER");
+        pass = prop.getProperty("DB_PASS");
+        schema = prop.getProperty("DB_SCHEMA");
+        driver = prop.getProperty("DB_DRIVER");
+        maxIdle = prop.getProperty("MAX_IDLE_CONNECTIONS");
+        maxOpen = prop.getProperty("MAX_OPEN_STATEMENTS");
+        this.createConnection(url, user, pass);
+        if(schema != null) {
+            this.setConnectionSchema(schema);
+        }
+        if(driver != null) {
+            this.setConnectionDriver(driver);
+        }
+        if(maxIdle != null) {
+            this.setMaxIdleConnections(Integer.parseInt(maxIdle));
+        }
+        if(maxOpen != null) {
+            this.setMaxOpenStatements(Integer.parseInt(maxOpen));
+        }
+    }
 
     public Configuration addAnnotatedClass(Class<?> clazz) {
         if(modelsList == null) modelsList = new ArrayList<>();

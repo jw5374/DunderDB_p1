0328ffb3299d1a605387adb69c7320d12489de19
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-13 10:47:12 -0400

    added savepoint release in transactions

--- dunderdborm/src/main/java/com/dunderdb/DunderTx.java
+++ dunderdborm/src/main/java/com/dunderdb/DunderTx.java
@@ -3,6 +3,8 @@ package com.dunderdb;
 public interface DunderTx {
      void savePoint(String name);
 
+     void releaseSavepoint(String name);
+
      void rollback(String name);
 
      void commit();
